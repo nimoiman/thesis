07cae11306a328b0d822dc99dd4ae73cac8a105b
jameh <jamie.alban@gmail.com>
2013-12-27 18:02:18 -0500

    Add output messages

--- vector_main.c
+++ vector_main.c
@@ -8,9 +8,11 @@ int main(int argc, char *argv[]){
 	if((train = init_vectorset(numtrain))){
 		for(i = 0; i < numtrain; i++)
 			for(j = 0; j < VECTOR_DIM; j++)
-				train->v[i][j] = i*j;
+				train->v[i][j] = (i+1)*(j+1);
+		printf("Training set:\n");
 		print_vectorset(train);
 		c = lbgvq(train, 2);
+		printf("Generated Codebook:\n");
 		print_vectorset(c);
 	}
 	else
