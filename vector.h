693e403f21dc0adb68fc9f3f1f4b8f5c6362d81c
jameh <jamie.alban@gmail.com>
2013-12-29 02:41:11 -0500

    Move common vector stuff to own files.

---
+++ vector.h
@@ -0,0 +1,30 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#include <stdio.h> /* sprintf */
+#include <stdlib.h> /* malloc, free, size_t */
+#include "vector.h"
+
+#define VECTOR_DIM 2
+
+typedef struct{
+    size_t size;
+    double (*v)[VECTOR_DIM];
+} vectorset;
+
+vectorset *init_vectorset(size_t size);
+void destroy_vectorset(vectorset *v);
+void print_vectorset(FILE *stream, vectorset *v);
+
+double dist(double *x, double *y);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VECTOR_H */
+
