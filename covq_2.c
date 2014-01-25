dd323165d0bc0e3d1fe1ecb91b5b415f6f449647
9pt7 <peter.thompson92@gmail.com>
2014-01-24 20:24:30 -0500

    added transition probability function

--- covq_2.c
+++ covq_2.c
@@ -1,3 +1,5 @@
+#include "covq_2.h"
+
 #define C_SIZE_X (1 << split_x)
 #define C_SIZE_Y (1 << split_y)
 
@@ -40,20 +42,37 @@ int quantize() {
 
 }
 
-/* source should be "X" or "Y" */
+/* Computes the channel transition probability from index i to index j for a
+ * given source (src). */
 double trans_prob(int i, int j, int src) {
+	double p = 1;
+	double prob;
+	int len, k, diff;
 
+	diff = i ^ j;
+	len = (src == X) ? split_x : split_y;
+	prob = (src == X) ? TRANS_PROB_X : TRANS_PROB_Y;
+	for(k = 0; k < len; k++)
+		p *= (diff>>i & 1) ? prob : (1-prob);
+	return p;
 }
 
+/* Computes the distortion between a source and code vector for a given source.
+ * The quantized level should be input, along with the index to the code
+ * vector, and the source.  Returns the expected mean squared distortion
+ * between q_lvl and the estimate at the source for the given index to be
+ * transmitted. */
 double dist(int q_lvl, int index, int src) {
-
 }
 
-/* return *index by reference */
+/* for a given source vector, returns the nearest code vector and respective
+ * distortion with respect to the dist() function. The source quantization
+ * level should be passed, along with a pointer which will point to the nearest
+ * code vector index, and the source. */
 double nearest_neighbour(int q_lvl, int *index, int src) {
-    // calls dist, trans_prob
 }
 
+/* Updates the centroids for a given source by applying the centroid condition. */
 double centroid_update(int src) {
     // calls nearest_neighbour, trans_prob
 }
