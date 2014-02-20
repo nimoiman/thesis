896cb7a6182ae29952639677d234b609551780d9
jameh <jamie.alban@gmail.com>
2014-02-20 15:51:33 -0500

    Fix the quantizer.

--- covq_2/quantize.c
+++ covq_2/quantize.c
@@ -1,77 +1,72 @@
 #include "covq.h"
 
-/* Convert 'vector' x to quantization level */
+/* Convert 'vector' x to quantization level assumes zero mean */
 int vec_to_quant(double x, int *outlier, int src) {
     double normalized;
     int q_lvls;
-    double mean, spread;
+    double q_length;
 
-    if (src == SRC_X) {
-        q_lvls = Q_LEVELS_X;
-        mean = mean_x[0];
-        spread = range_x[0];
-    }
-    else { // src == SRC_Y
-        q_lvls = Q_LEVELS_Y;
-        mean = mean_y[0];
-        spread = range_y[0];
-    }
+    q_length = (src == SRC_X) ? Q_LENGTH_X : Q_LENGTH_Y;
 
-    normalized = 0.5 * (((x - mean) / (spread)) + 1);
-    if (x - mean < spread) {
+    // normalize to [0, 1)
+    normalized = (x / q_length) + 0.5;
+    if (normalized < 0) {
         *outlier = 1;
         return 0;
     }
-    else if (x - mean > spread) {
+    else if (normalized >= 1) {
         *outlier = 1;
-        return (q_lvls - 1);
+        return (Q_LEVELS - 1);
     }
     else {
-        normalized = 0.5 * (((x - mean) / spread) + 1);
-        return (int) (q_lvls * normalized);
+        // within range [0, Q_LEVELS-1]
+        return (int) (Q_LEVELS * normalized);
     }
 }
 
-/* Return center of quantization region indexed by x */
+/* Return vector centroid of quantization region indexed by x */
 double quant_to_vec(int x, int src) {
+    double normalized;
+    normalized = ((double) x + 0.5) / Q_LEVELS;
     if (src == SRC_X) {
-        return range_x[0] * ((((double) (2 * x) + 1) / Q_LEVELS_X) - 1) + mean_x[0];
+        return Q_LENGTH_X * normalized;
     }
     else { // src == SRC_Y
-        return range_y[0] * ((((double) (2 * x) + 1) / Q_LEVELS_Y) - 1) + mean_y[0];
+        return Q_LENGTH_Y * normalized;
     }
 }
 
-/* Store quantize bin counts of quantized training vectors into q_tr
+/* Store quantize bin counts of quantized training vectors into q_trset
  *
  * return number of vectors outside quantize region
- * & puts vectors outside of quantize region in nearest bin
+ * & discards vectors outside of quantize region
  * note: (Vector is outlier) ~ Geometric(p, N) where p is the probability of
  * a given component being outside the quantize region
  */
 int quantize() {
     int i, j, dim; // iteration variables
     int x_bin, y_bin;
-    int outlier = 0;
     int outlier_count = 0;
 
     // initialize quantized bin counts to 0
-    for (i = 0; i < Q_LEVELS_X; i++) {
-        for (j = 0; j < Q_LEVELS_Y; j++) {
-            q_tr[i][j] = 0;
+    for (i = 0; i < Q_LEVELS; i++) {
+        for (j = 0; j < Q_LEVELS; j++) {
+            q_trset[i][j] = 0;
         }
     }
 
     // iterate through X and Y jointly
     for (i = 0; i < tr_size; i++) {
-        x_bin = vec_to_quant(tr_x[i], &outlier, SRC_X);
-        y_bin = vec_to_quant(tr_y[i], &outlier, SRC_Y);
-        q_tr[x_bin][y_bin] += 1;
-
-        if (outlier) {
+        // check if training vector an outlier
+        if (trset_x[i] < Q_LENGTH_X || Q_LENGTH_X <= trset_x[i] ||
+            trset_y[i] < Q_LENGTH_Y || Q_LENGTH_Y <= trset_y[i]) {
+            // discard this training vector
             outlier_count++;
-            outlier = 0;
+            continue;
         }
+        x_bin = vec_to_quant(trset_x[i], NULL, SRC_X);
+        y_bin = vec_to_quant(trset_y[i], NULL, SRC_Y);
+        q_trset[x_bin][y_bin] += 1;
     }
 
     return outlier_count;
