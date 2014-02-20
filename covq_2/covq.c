8d11cea9126aa24e4845334d77d41d3948f4675c
9pt7 <peter.thompson92@gmail.com>
2014-02-20 15:02:08 -0500

    finished NN and CC

--- covq_2/covq.c
+++ covq_2/covq.c
@@ -1,6 +1,7 @@
 #include "covq.h"
+#define POW2(x) (x*x)
 
-double trans_prob(int i, int j, int k, int l) {
+double channel_prob(int i, int j, int k, int l) {
     double p = 1;
     int n, diff;
     int i_cw, j_cw, k_cw, l_cw;
@@ -18,146 +19,143 @@ double trans_prob(int i, int j, int k, int l) {
     return p;
 }
 
-/* for a given source vector, returns the nearest code vector and respective
- * distortion with respect to the dist() function. The source quantization
- * level should be passed, along with a pointer which will point to the nearest
- * code vector index, and the source. */
 double nearest_neighbour(int q_lvl, int *index, int src) {
-    int src_count[CODEBOOK_SIZE_MAX];
-    double src_sum[CODEBOOK_SIZE_MAX], rcv_prob[CODEBOOK_SIZE_MAX], rcv_avg[CODEBOOK_SIZE_MAX];
-    double p, d, d_best, var, val, d_term, *dist;
-    int i, j, k, l, src1, src2, num, c, count, i_best, c_size1, c_size2, *enc2;
-    double (*c1)[FINAL_C_SIZE_Y], (*c2)[FINAL_C_SIZE_Y];
+	double src_prob[CODEBOOK_SIZE_MAX], src_expected_val[CODEBOOK_SIZE_MAX];
+	double d, d_best, src_var, val;
+	int i, j, k, l, src1, src2, num, count, c_size1, c_size2, *enc2;
+	codevectors *c1, *c2;
 
-    // set primary and secondary source parameters
-    if(src == SRC_X){
-        c_size1 = C_SIZE_X;
-        c_size2 = C_SIZE_Y;
-        c1 = c_x;
-        c2 = c_y;
-        src1 = SRC_X;
-        src2 = SRC_Y;
-        enc2 = enc_y;
-    }else{
+	val = quant_to_vec(q_lvl, src);
 
-    }
-    
-    // for src1 == SRC_X, compute:
-    // E[Y^2 | X = x] (var)
-    // number of points that map to each J index (src_count)
-    // sum of points that map to each J index (src_sum)
-    for(i = 0; i < Q_LEVELS; i++){
-        if (src1 == SRC_X) {
-            num = q_tr[q_lvl][i];
-        }
-        else {
-            num = q_tr[i][q_lvl];
-        }
-        c = enc2[i];
-        val = quant_to_vec(i, src2);
+	// set primary and secondary source parameters
+	if(src == SRC_X){
+		c_size1 = CODEBOOK_SIZE_X;
+		c_size2 = CODEBOOK_SIZE_Y;
+		c1 = &cv_x;
+		c2 = &cv_y;
+		src1 = SRC_X;
+		src2 = SRC_Y;
+		enc2 = encoder_y;
+	}else{
+		c_size1 = CODEBOOK_SIZE_Y;
+		c_size2 = CODEBOOK_SIZE_X;
+		c1 = &cv_y;
+		c2 = &cv_x;
+		src1 = SRC_Y;
+		src2 = SRC_X;
+		enc2 = encoder_x;
+	}
+	
+	for(i = 0; i < Q_LEVELS; i++){
+		if(src1 == SRC_X)
+			num = q_trset[q_lvl][i];
+		else
+			num = q_trset[i][q_lvl];
+		j = enc2[i]; // Encoding index
+		val = quant_to_vec(i, src2); // value
 
-        count += num;
-        src_count[c] += num;
-        src_sum[c] += num * val;
-        var += num * val * val;
-    }
-    var /= count;
-    
-    // for src1 == SRC_X, compute:
-    // P(L = l | X = x) for all l (rcv_prob)
-    // E[Y | L = l, X = x] for all l (rcv_avg)
-    // TODO check above (rcv_avg)
-    for(l = 0; l < c_size2; l++){
-        for(j = 0; j < c_size2; j++){
-            p = trans_prob(j,l, src2);
-            rcv_prob[l] += p * src_count[j];
-            rcv_avg[l] += p * src_sum[j];
-        }
-        rcv_prob[l] /= count;
-        rcv_avg[l] /= count;
-    }
+		count += num;
+		src_prob[j] += num;
+		src_expected_val[j] += num * val;
+		src_var += num * val * val;
+	}
+	src_var /= count;
+	for(j = 0; j < c_size2; j++){
+		src_expected_val[j] /= src_prob[j];
+		src_prob[j] /= count;
+	}
 
-    val = quant_to_vec(q_lvl, src1); //Get x value from quantizer index
-    dist = src_sum; // reuse src_sum for new array, dist
-    for(k = 0; k < c_size1; k++){
-        dist[k] = 0;
-        for(l = 0; l < c_size2; l++){
-            d_term = (val-c1[k][l]) * (val-c1[k][l])
-                + c2[k][l] * c2[k][l]
-                - 2 * c2[k][l] * rcv_avg[l];
-            dist[k] += rcv_prob[l] * d_term;
-        }
-    }
-
-    for(i = 0; i < c_size1; i++){
-        d = 0;
-        for(k = 0; k < c_size1; k++)
-            d += trans_prob(i,k,src1) * dist[k];
-        if( d < d_best ){
-            d_best = d;
-            i_best = i;
-        }
-    }
-
-    *index = i_best;
-    return var + d_best;
+	d_best = -1;
+	*index = 0;	
+	for(i = 0; i < c_size1; i++){
+		d = src_var;
+		for(j = 0; j < c_size1; j++)
+			for(k = 0; k < c_size1; k++)
+				for(l = 0; l < c_size1; l++)
+					d += (POW2(val - (*c1)[k][l])
+							-2 * (*c2)[k][l] * src_expected_val[j]
+							+ POW2((*c2)[k][l]))
+						*src_prob[j] * channel_prob(i,j,k,l);
+		if(d_best < 0 || d < d_best){
+			d_best = d;
+			*index = i;
+		}
+	}
+	return d_best;
 }
 
 double nn_update(){
-    double d_total, d;
-    int i, j, num;
-    
-    //TODO check dist. equation
-    for(i = 0; i < Q_LEVELS; i++){
-        d = nearest_neighbour(i, enc_x + i, SRC_X);
-        num = 0;
-        for(j = 0; j < Q_LEVELS; j++)
-            num += q_tr[i][j];
-        d_total += num * d;
-    }
+	double d_total, d;
+	int i, j, num;
+	
+	//TODO check dist. equation
+	for(i = 0; i < Q_LEVELS; i++){
+		d = nearest_neighbour(i, encoder_x + i, SRC_X);
+		num = 0;
+		for(j = 0; j < Q_LEVELS; j++)
+			num += q_trset[i][j];
+		d_total += num * d;
+	}
 
-    for(j = 0; j < Q_LEVELS; j++){
-        d = nearest_neighbour(j, enc_y + j, SRC_Y);
-        num = 0;
-        for(i = 0; i < Q_LEVELS; i++)
-            num += q_tr[i][j];
-        d_total += num * d;
-    }
+	for(j = 0; j < Q_LEVELS; j++){
+		d = nearest_neighbour(j, encoder_y + j, SRC_Y);
+		num = 0;
+		for(i = 0; i < Q_LEVELS; i++)
+			num += q_trset[i][j];
+		d_total += num * d;
+	}
 
-    return d_total / tr_size;
+	return d_total / trset_size;
 }
 
 /* Updates the centroids for a given source by applying the centroid condition. */
 void centroid_update(int src) {
-    int count[C_SIZE_X][C_SIZE_Y];
-    double sum[C_SIZE_X][C_SIZE_Y];
-    int num, i, j, k, l, idx_x, idx_y;
-    double c, val_x, d_x, d_y, d_total, p_x, p_y, numer, denom;
+	int count[CODEBOOK_SIZE_X][CODEBOOK_SIZE_Y];
+	double sum[CODEBOOK_SIZE_X][CODEBOOK_SIZE_Y];
+	int num, i, j, k, l, q_val1, q_val2, src1, src2, *enc1, *enc2, c_size1, c_size2;
+	double c, val, p, numer, denom;
+    codevectors *cv1;
 
-    for(i = 0; i < C_SIZE_X; i++){
-        idx_x = enc_x[i];
-        val_x = quant_to_vec(i, src);
-        for(j = 0; j < C_SIZE_Y; j++){
-            idx_y = enc_y[j];
-            num = q_tr[i][j];
-            count[idx_x][idx_y] += num;
-            sum[idx_x][idx_y] += num * val_x;
+	if(src == SRC_X){
+		c_size1 = CODEBOOK_SIZE_X;
+		c_size2 = CODEBOOK_SIZE_Y;
+		enc1 = encoder_x;
+		enc2 = encoder_y;
+        cv1 = &cv_x;
+	}else{
+		c_size1 = CODEBOOK_SIZE_Y;
+		c_size2 = CODEBOOK_SIZE_X;
+		enc1 = encoder_y;
+		enc2 = encoder_x;
+        cv1 = &cv_y;
+	}
+    
+    for(q_val1 = 0; q_val1 < Q_LEVELS; q_val1++){
+        i = enc1[q_val1];
+        val = quant_to_vec(q_val1, src);
+        for(q_val2 = 0; q_val2 < Q_LEVELS; q_val2++){
+            j = enc2[q_val2];
+            if(src1 == SRC_X)
+                num = q_trset[q_val1][q_val2];
+            else
+                num = q_trset[q_val2][q_val1];
+            count[i][j] += num;
+            sum[i][j] += num * val;
         }
     }
 
-    for(k = 0; k < C_SIZE_X; k++){
-        for(l = 0; l < C_SIZE_Y; l++){
-            numer = 0;
-            denom = 0;
-            for(i = 0; i < C_SIZE_X; i++){
-                p_x = trans_prob(i,k,SRC_X);
-                for(j = 0; j < C_SIZE_Y; j++){
-                    p_y = trans_prob(j,l,SRC_Y);
-                    numer += p_x * p_y * sum[i][j];
-                    denom += p_x * p_y * count[i][j];
-                }
-            }
-            c_x[k][l] = numer / denom;
-        }
-    }
+	for(k = 0; k < c_size1; k++){
+		for(l = 0; l < c_size2; l++){
+			numer = 0;
+			denom = 0;
+			for(i = 0; i < c_size1; i++){
+				for(j = 0; j < c_size2; j++){
+                    p = channel_prob(i,j,k,l);
+					numer += p * sum[i][j];
+					denom += p * count[i][j];
+				}
+			}
+			(*cv1)[k][l] = numer / denom;
+		}
+	}
 }
--- covq_2/running.c
+++ covq_2/running.c
@@ -1,29 +1,6 @@
-#include "covq_2.h"
+#include "covq.h"
 
-int init(struct covq *c) {
-    // unpack everything
-    tr_size = c->tr_size;
-    tr_x = c->tr_x;
-    tr_y = c->tr_y;
-
-    q_tr = c->q_tr;
-
-    c_x = c->c_x;
-    c_y = c->c_y;
-
-    split_x = c->split_x;
-    split_y = c->split_y;
-
-    enc_x = c->enc_x;
-    enc_y = c->enc_y;
-
-    sigma_x = c->sigma_x;
-    sigma_y = c->sigma_y;
-    mean_x = c->mean_x;
-    mean_y = c->mean_y;
-
-    enc_x = malloc(sizeof(int) * Q_LEVELS_X);
-    enc_y = malloc(sizeof(int) * Q_LEVELS_Y);
+int init(void) {
 
     // quantize
     quantize();
