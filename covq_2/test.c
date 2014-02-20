00620956a8f09cd1a6e6aa0c53b8493ea2af3359
9pt7 <peter.thompson92@gmail.com>
2014-02-20 16:07:54 -0500

    added test file and print test

---
+++ covq_2/test.c
@@ -0,0 +1,25 @@
+#include "covq.h"
+
+void print_test(void){
+    int i;
+    int int_arr[25];
+    double double_arr[25];
+    
+    printf("testing print int array...\n");
+
+    for( i = 0; i < 25; i++)
+        int_arr[i] = i+1;
+
+    print_int_array(stdout, int_arr, 25);
+    printf("\n");
+    
+    for(i = 0; i < 25; i++)
+        double_arr[i]=((double)rand()/(double)RAND_MAX);
+    
+    printf("testing print double array...\n");
+    print_double_array(stdout, double_arr, 25);
+    printf("\n");
+}
+int main( int argc, const char* argv[] ){
+    print_test();
+}
