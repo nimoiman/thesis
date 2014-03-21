d4826b642673e209e1fac26568073c9ef5bc50e1
jameh <jamie.alban@gmail.com>
2014-03-20 20:44:35 -0400

    Move csv I/O functions to util.py

--- python/image.py
+++ python/image.py
@@ -6,17 +6,9 @@ and testing
 # Python 2 compat
 from __future__ import print_function
 
-import sys
-import traceback
-
 from api_docs import command, parse_args, help
+from util import *
 
-def _exit(msg=1):
-    traceback.print_stack()
-    raise SystemExit(msg)
-
-def _stderr(msg):
-    print(msg, file=sys.stderr)
 
 def _2d_dct(a):
     """Perform 2d DCT-II on a in row, column order"""
@@ -32,144 +24,12 @@ def _2d_idct(a):
     return idct(idct(a.transpose(), norm='ortho').transpose(), norm='ortho')
 
 
-def csv_read(filename, dims, block_order=False, block_size=None):
-    """Read a single column newline-separated file of floats, return
-    a list of numpy arrays of dimensions specified by dims
-    .. note::
-        The single column newline-separated file is the standard that
-        we use because we wrote C code that likes it that way.
-    """
-
-    # Check that num lines == dim_x * dim_y
-    # Find how many lines in csv_filename
-    import numpy as np
-    lines = 0
-    for line in open(filename, 'r'):
-        lines += 1
-
-    if lines != sum([dim[0] * dim[1] for dim in dims]):
-        _stderr("The provided dimensions do not match the number of lines " +
-              "in the input file")
-        _exit(1)
-
-    arrs = []
-    for dim in dims:
-        
-        if not block_order:
-            arr = np.empty(dim[0] * dim[1], dtype=float)
-            with open(filename, 'r') as f:
-                for i in range(dim[0] * dim[1]):
-                    arr[i] = float(f.readline())
-
-            arr = arr.reshape((dim[0], dim[1]))
-            arrs.append(arr)
-        else: # block_order
-            arr = np.empty(dim, dtype=float)
-            
-            if block_size is not None:
-                block_size = int(block_size)
-                block_shape = (block_size, block_size)
-                # Read as block by block serial data
-                with open(filename, 'r') as f:
-                    for block in iter_array(arr, block_shape):
-                        for i in range(block_size):
-                            for j in range(block_size):
-                                arr[i,j] = float(f.readline())
-            else: # block_size is None
-                raise TypeError("No block size provided with option " +
-                                "block_order=True")
-            
-    return arrs
-
-
-def csv_write(filename, arr, mode='w', block_order=False,
-              block_size=None):
-    """Write a single column newline-separated file of floats from list
-    of 2D numpy arrays :param:`arrs`
-    """
-
-    if len(arr.shape) != 2:
-        raise ValueError('array should have dimension 2')
-
-    if not block_order:
-        with open(filename, mode) as f:
-            for i in range(arr.shape[0]):
-                for j in range(arr.shape[1]):
-                    f.write(str(arr[i,j]) + "\n")
-    else: # block_order
-        if block_size is not None:
-            with open(filename, mode) as f:
-                for block in iter_array(arr, (block_size, block_size)):
-                    for i in range(block_size):
-                        for j in range(block_size):
-                            f.write(str(arr[i,j]) + "\n")
-        else: # block_size is None
-            raise TypeError('block_size must be specified for block_order ' +
-                            'output')
-
-
-def iter_array(arr, vec_shape=(8,1)):
-    """Iterate through numpy array arr in left-to-right, then
-    top-to-bottom order, outputting windowed views of arr of shape
-    vec_shape
-
-    .. note::
-        Any change you make to the outputted views changes the original
-        array arr
-    """
-
-    # Make sure vec_shape matches arr shape
-    if arr.shape[0] % vec_shape[0] != 0 or arr.shape[1] % vec_shape[1] != 0:
-        raise ValueError('dimension mismatch')
-
-    for i in range(0, arr.shape[0], vec_shape[0]):
-        for j in range(0, arr.shape[1], vec_shape[1]):
-            yield arr[i:i+vec_shape[0], j:j+vec_shape[1]]
-
-
 def _count_lines(f):
     """Return number of newlines in f"""
     n_lines = 0
     for line in open(f):
         n_lines += 1
     return n_lines
-
-
-@command
-def csv_zip(filename_1, filename_2, out_file):
-    """Zip two files together line-by-line"""
-
-    # Check that the number of lines is the same
-    if _count_lines(filename_1) != _count_lines(filename_2):
-        _stderr("Files provided do not have the same number of lines")
-        _exit(1)
-
-    lines = []
-    with open(filename_1, 'r') as f_1:
-        lines.append(f_1.readlines())
-    with open(filename_2, 'r') as f_2:
-        lines.append(f_2.readlines())
-
-    with open(out_file, 'w') as f:
-        for zipped_line in zip(lines[0], lines[1]):
-            f.write("{},{}\n".format(zipped_line[0].strip(),
-                                     zipped_line[1].strip()))
-
-
-@command
-def csv_unzip(filename, out_file_1, out_file_2, slice_index=1):
-    """Unzip file into two files containing slice_index, and
-    (original dim - slice_index) columns in each respectively"""
-    with open(filename, 'r') as f:
-        zipped_lines = [l.strip() for l in f.readlines()]
-
-    with open(out_file_1, 'w') as f_1:
-        for zipped_line in zipped_lines:
-            f_1.write(','.join(zipped_line.split(',')[:slice_index]) + '\n')
-
-    with open(out_file_2, 'w') as f_2:
-        for zipped_line in zipped_lines:
-            f_2.write(','.join(zipped_line.split(',')[slice_index:]) + '\n')
 
 
 def ster2arr(filename_1, filename_2, block_size=8):
@@ -219,36 +79,6 @@ def _img_dct(img_1, img_2, block_size=8):
     return ster_images
 
 
-@command
-def ster2csv(filename_1, filename_2, out_filename_1, out_filename_2,
-             mode='w', block_order=False, block_size=8):
-    """Extract pixel data from stereo grayscale images filename_1,
-    filename_2, perform normalized DCT-II on block_size x block_size
-    blocks and output to 2 csv files of DCT coefficients
-
-    .. note::
-        If size of images is not multiple of block_size, will crop from
-        the right and bottom such that the two images have dimensions
-        which are multiples of block_size, and are the same size.
-
-    If block_order=True, serialize data per block first.
-    """
-
-    import numpy as np
-
-    ster_images, dim = ster2arr(filename_1, filename_2, block_size=block_size)
-
-    ster_images = _img_dct(ster_images[0], ster_images[1],
-                           block_size=block_size)
-
-    csv_write(out_filename_1, ster_images[0], mode=mode,
-              block_order=block_order, block_size=block_size)
-    csv_write(out_filename_2, ster_images[1], mode=mode,
-              block_order=block_order, block_size=block_size)
-
-    return dim
-
-
 def _var_block(imgs, block_size):
     """Return matrix of Variances of blocks of block_size in training
     set of images :param:`imgs`
@@ -280,11 +110,13 @@ def _var_block(imgs, block_size):
 
     return var_block
 
+
 def dist_rate_gauss(var, n_bits):
     """Return distortion-rate function for Gaussian source with
     rate :param:`n_bits`, variance :param:`var`"""
 
     return var / (2 ** (2*n_bits))
+
 
 def dist_rate_laplace(var, delta):
     """Return distortion-rate approximation for Laplacian source with
@@ -349,13 +181,13 @@ def bit_allocate(csv_filename_1, csv_filename_2, block_bit_rate, dims,
     n_lines_2 = _count_lines(csv_filename_2)
 
     if n_lines_1 != n_lines_2:
-        _stderr("size of input csvs don't match")
-        _exit(1)
+        stderr_("size of input csvs don't match")
+        exit_(1)
 
     if n_lines_1 % (block_size * block_size) != 0:
-        _stderr("block_size does not divide the number of lines in " +
+        stderr_("block_size does not divide the number of lines in " +
               "the csv file.")
-        _exit(1)
+        exit_(1)
 
     ster_images = []
 
@@ -392,6 +224,37 @@ def bit_allocate(csv_filename_1, csv_filename_2, block_bit_rate, dims,
 
     return (bit_allocs, var_blocks)
 
+
+@command
+def ster2csv(filename_1, filename_2, out_filename_1, out_filename_2,
+             mode='w', block_order=False, block_size=8):
+    """Extract pixel data from stereo grayscale images filename_1,
+    filename_2, perform normalized DCT-II on block_size x block_size
+    blocks and output to 2 csv files of DCT coefficients
+
+    .. note::
+        If size of images is not multiple of block_size, will crop from
+        the right and bottom such that the two images have dimensions
+        which are multiples of block_size, and are the same size.
+
+    If block_order=True, serialize data per block first.
+    """
+
+    import numpy as np
+
+    ster_images, dim = ster2arr(filename_1, filename_2, block_size=block_size)
+
+    ster_images = _img_dct(ster_images[0], ster_images[1],
+                           block_size=block_size)
+
+    csv_write(out_filename_1, ster_images[0], mode=mode,
+              block_order=block_order, block_size=block_size)
+    csv_write(out_filename_2, ster_images[1], mode=mode,
+              block_order=block_order, block_size=block_size)
+
+    return dim
+
+
 def csv2ster(csv_filename_1, csv_filename_2, out_filenames_1, out_filenames_2,
              dims, block_order=False, block_size=8):
     """Extract DCT coefficiencts from 2-column csv_filename, perform 
@@ -406,24 +269,24 @@ def csv2ster(csv_filename_1, csv_filename_2, out_filenames_1, out_filenames_2,
     lines_2 = _count_lines(csv_filename_2)
 
     if lines_1 != lines_2:
-        _stderr("The provided csv files do not have the same number of " +
+        stderr_("The provided csv files do not have the same number of " +
                 "lines.")
-        _exit(1)
+        exit_(1)
 
     if lines_1 != sum([dim[0] * dim[1] for dim in dims]):
-        _stderr("The provided dimensions do not match the number of lines " +
+        stderr_("The provided dimensions do not match the number of lines " +
                 "in the input files")
-        _stderr("file1: {}".format(csv_filename_1))
-        _stderr("file2: {}".format(csv_filename_2))
-        _stderr("# of lines: {}".format(lines_1))
-        _stderr("dimensions: {}".format(dims))
-        _exit(1)
+        stderr_("file1: {}".format(csv_filename_1))
+        stderr_("file2: {}".format(csv_filename_2))
+        stderr_("# of lines: {}".format(lines_1))
+        stderr_("dimensions: {}".format(dims))
+        exit_(1)
     else:
         for dim in dims:
             if dim[0] % block_size != 0 or dim[1] % block_size != 0:
-                _stderr("The provided dimensions must be multiples of block " +
+                stderr_("The provided dimensions must be multiples of block " +
                         "size {}".format(block_size))
-                _exit(1)
+                exit_(1)
 
     ster_images = []
     ster_images.append(csv_read(csv_filename_1, dims, block_order, block_size))
---
+++ python/util.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+"""A collection of utilities for csv I/O for use with COVQ training
+and testing, and some generic Python I/O utility functions
+"""
+
+# Python 2 compat
+from __future__ import print_function
+
+import traceback, sys
+from api_docs import command, parse_args, help
+
+
+def exit_(msg=1):
+    traceback.print_stack()
+    raise SystemExit(msg)
+
+
+def stderr_(msg):
+    print(msg, file=sys.stderr)
+
+
+def iter_array(arr, vec_shape=(8,1)):
+    """Iterate through numpy array arr in left-to-right, then
+    top-to-bottom order, outputting windowed views of arr of shape
+    vec_shape
+
+    .. note::
+        Any change you make to the outputted views changes the original
+        array arr
+    """
+
+    # Make sure vec_shape matches arr shape
+    if arr.shape[0] % vec_shape[0] != 0 or arr.shape[1] % vec_shape[1] != 0:
+        raise ValueError('dimension mismatch')
+
+    for i in range(0, arr.shape[0], vec_shape[0]):
+        for j in range(0, arr.shape[1], vec_shape[1]):
+            yield arr[i:i+vec_shape[0], j:j+vec_shape[1]]
+
+
+def csv_read(filename, dims, block_order=False, block_size=None):
+    """Read a single column newline-separated file of floats, return
+    a list of numpy arrays of dimensions specified by dims
+    .. note::
+        The single column newline-separated file is the standard that
+        we use because we wrote C code that likes it that way.
+    """
+
+    # Check that num lines == dim_x * dim_y
+    # Find how many lines in csv_filename
+    import numpy as np
+    lines = 0
+    for line in open(filename, 'r'):
+        lines += 1
+
+    if lines != sum([dim[0] * dim[1] for dim in dims]):
+        stderr_("The provided dimensions do not match the number of lines " +
+              "in the input file")
+        exit_(1)
+
+    arrs = []
+    for dim in dims:
+        
+        if not block_order:
+            arr = np.empty(dim[0] * dim[1], dtype=float)
+            with open(filename, 'r') as f:
+                for i in range(dim[0] * dim[1]):
+                    arr[i] = float(f.readline())
+
+            arr = arr.reshape((dim[0], dim[1]))
+            arrs.append(arr)
+        else: # block_order
+            arr = np.empty(dim, dtype=float)
+            
+            if block_size is not None:
+                block_size = int(block_size)
+                block_shape = (block_size, block_size)
+                # Read as block by block serial data
+                with open(filename, 'r') as f:
+                    for block in iter_array(arr, block_shape):
+                        for i in range(block_size):
+                            for j in range(block_size):
+                                arr[i,j] = float(f.readline())
+            else: # block_size is None
+                raise TypeError("No block size provided with option " +
+                                "block_order=True")
+            
+    return arrs
+
+
+def csv_write(filename, arr, mode='w', block_order=False,
+              block_size=None):
+    """Write a single column newline-separated file of floats from list
+    of 2D numpy arrays :param:`arrs`
+    """
+
+    if len(arr.shape) != 2:
+        raise ValueError('array should have dimension 2')
+
+    if not block_order:
+        with open(filename, mode) as f:
+            for i in range(arr.shape[0]):
+                for j in range(arr.shape[1]):
+                    f.write(str(arr[i,j]) + "\n")
+    else: # block_order
+        if block_size is not None:
+            with open(filename, mode) as f:
+                for block in iter_array(arr, (block_size, block_size)):
+                    for i in range(block_size):
+                        for j in range(block_size):
+                            f.write(str(arr[i,j]) + "\n")
+        else: # block_size is None
+            raise TypeError('block_size must be specified for block_order ' +
+                            'output')
+
+
+@command
+def csv_zip(filename_1, filename_2, out_file):
+    """Zip two files together line-by-line"""
+
+    # Check that the number of lines is the same
+    if _count_lines(filename_1) != _count_lines(filename_2):
+        stderr_("Files provided do not have the same number of lines")
+        exit_(1)
+
+    lines = []
+    with open(filename_1, 'r') as f_1:
+        lines.append(f_1.readlines())
+    with open(filename_2, 'r') as f_2:
+        lines.append(f_2.readlines())
+
+    with open(out_file, 'w') as f:
+        for zipped_line in zip(lines[0], lines[1]):
+            f.write("{},{}\n".format(zipped_line[0].strip(),
+                                     zipped_line[1].strip()))
+
+
+@command
+def csv_unzip(filename, out_file_1, out_file_2, slice_index=1):
+    """Unzip file into two files containing slice_index, and
+    (original dim - slice_index) columns in each respectively"""
+    with open(filename, 'r') as f:
+        zipped_lines = [l.strip() for l in f.readlines()]
+
+    with open(out_file_1, 'w') as f_1:
+        for zipped_line in zipped_lines:
+            f_1.write(','.join(zipped_line.split(',')[:slice_index]) + '\n')
+
+    with open(out_file_2, 'w') as f_2:
+        for zipped_line in zipped_lines:
+            f_2.write(','.join(zipped_line.split(',')[slice_index:]) + '\n')
+
+if __name__ == '__main__':
+    parse_args()
