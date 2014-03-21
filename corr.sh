1b2448ca710eaaaac5b422e91d21568e838b3f63
jameh <jamie.alban@gmail.com>
2014-03-21 12:07:35 -0400

    Modify corr.sh to work with Peters data

--- corr.sh
+++ corr.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/bash
 make covq
 
-for i in {1..9}
+for i in {0..9}
 do
-    python python/util.py csv_unzip training_set_$i.csv tr_left.csv tr_right.csv
-    python python/util.py csv_unzip testing_set_$i.csv test_left.csv test_right.csv
+for j in {0..9}
+do
+    python python/util.py csv_unzip dropbox/datasets/mydatasets/train_0.$i$j.csv tr_left.csv tr_right.csv
+    python python/util.py csv_unzip dropbox/datasets/mydatasets/sim_0.$i$j.csv test_left.csv test_right.csv
     ./covq/covq
 done
+done
+python python/util.py csv_unzip dropbox/datasets/mydatasets/train_1.00.csv tr_left.csv tr_right.csv
+python python/util.py csv_unzip dropbox/datasets/mydatasets/sim_1.00.csv test_left.csv test_right.csv
+./covq/covq
