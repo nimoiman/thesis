f946fe5f35ddf0d96a763972df6665c24bfe7789
jameh <jamie.alban@gmail.com>
2014-03-21 00:54:48 -0400

    Moved Python function to util.py

--- corr.sh
+++ corr.sh
@@ -3,7 +3,7 @@ make covq
 
 for i in {1..9}
 do
-    python python/image.py csv_unzip training_set_$i.csv tr_left.csv tr_right.csv
-    python python/image.py csv_unzip testing_set_$i.csv test_left.csv test_right.csv
+    python python/util.py csv_unzip training_set_$i.csv tr_left.csv tr_right.csv
+    python python/util.py csv_unzip testing_set_$i.csv test_left.csv test_right.csv
     ./covq/covq
 done
