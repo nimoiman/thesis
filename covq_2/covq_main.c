39894f78ace0e50f68795c795e07b568badffccb
jameh <jamie.alban@gmail.com>
2014-03-27 14:49:35 -0400

    Reduce scope of variables

--- covq_2/covq_main.c
+++ covq_2/covq_main.c
@@ -34,22 +34,19 @@ int B_X, B_Y;
 double trans_prob(int i, int j, int k, int l, int *b_X, int *b_Y)
 {
     double prob = 1;
-    int n, diff;
-    int i_cw, j_cw, k_cw, l_cw;
-
 
     // Get binary transmission codewords for the indexes
-    i_cw = b_X[i];
-    j_cw = b_Y[j];
-    k_cw = b_X[k];
-    l_cw = b_Y[l];
+    int i_cw = b_X[i];
+    int j_cw = b_Y[j];
+    int k_cw = b_X[k];
+    int l_cw = b_Y[l];
     
     // Compute transition probability
-    diff = i_cw ^ k_cw;
-    for(n = 0; n < B_X; n++)
+    int diff = i_cw ^ k_cw;
+    for(int n = 0; n < B_X; n++)
         prob *= (diff>>n & 1) ? trans_prob_x: (1-trans_prob_x);
     diff = j_cw ^ l_cw;
-    for(n = 0; n < B_Y; n++)
+    for(int n = 0; n < B_Y; n++)
         prob *= (diff>>n & 1) ? trans_prob_y: (1-trans_prob_y);
     
     return prob;
@@ -57,18 +54,15 @@ double trans_prob(int i, int j, int k, int l, int *b_X, int *b_Y)
 
 double run(int L_X, int L_Y, int N_X, int N_Y)
 {
-    FILE * pFile;
     covq2 v;
     unif_quant q;
-    
-    
 
     quantizer_init( &q, L_X, L_Y, -1, 1, -1, 1);
 
     /*
      * Read training set from file.
      */
-    pFile = fopen(TRSET_FILE, "r");
+    FILE *pFile = fopen(TRSET_FILE, "r");
     if( !pFile ){
         fprintf(stderr, "Could not open training set file.\n");
         return 0;
@@ -112,24 +106,21 @@ double run(int L_X, int L_Y, int N_X, int N_Y)
 
 int main()
 {
-    int N_X, N_Y;
     int L_X, L_Y;
-    double SQNR;
     int N_X_final, N_Y_final;
 
-    N_X_final = 32;
-    N_Y_final = 32;
-
+    N_X_final = N_Y_final = 32;
     L_X = L_Y = 100;
 
     printf("\t");
-    for(N_X = 1; N_X <= N_X_final; N_X *= 2)
+    for (int N_X = 1; N_X <= N_X_final; N_X *= 2) {
         printf("%d\t\t", N_X);
+    }
 
-    for(N_Y = 1; N_Y <= N_Y_final; N_Y *= 2){
+    for (int N_Y = 1; N_Y <= N_Y_final; N_Y *= 2) {
         printf("\n%d", N_Y);
-        for(N_X = 1; N_X <= N_X_final; N_X *= 2){
-            SQNR = run(L_X, L_Y, N_X, N_Y);
+        for (int N_X = 1; N_X <= N_X_final; N_X *= 2) {
+            double SQNR = run(L_X, L_Y, N_X, N_Y);
             printf("\t%f",SQNR);
         }
     }
