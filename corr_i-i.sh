139828ee9ff36d33ab0485c40321ff1975b1ca6c
jameh <jamie.alban@gmail.com>
2014-03-30 23:16:42 -0400

    Add error checking, clean up after yo'self

--- corr_i-i.sh
+++ corr_i-i.sh
@@ -7,7 +7,14 @@ nsplits=2
 bep=0.0
 lbg_eps=0.001
 cv_disp=0.0001
-
+tr_1=tr_1.csv
+tr_2=tr_2.csv
+sim_1=sim_1.csv
+sim_2=sim_2.csv
+out_1=out_1.csv
+out_2=out_2.csv
+cb=cb.csv
+cw=cw_map.csv
 
 
 # Clear SNR output file
@@ -18,14 +25,21 @@ for i in {0..9}
 do
     for j in 0 5
     do
+        train=corr_$k.$i$j.train
+        sim=corr_$k.$i$j.sim
+        if [ ! -e train ] || [ ! -e sim ]
+            then echo "Cannot find train/sim files, rho=$k.$i$j"
+            exit 1
+        fi
+
         # Split the sets into single columns
-        python python/util.py csv_unzip corr_$k.$i$j.train tr_1.csv tr_2.csv
-        python python/util.py csv_unzip corr_$k.$i$j.sim sim_1.csv sim_2.csv
+        python python/util.py csv_unzip $train $tr_1 $tr_2
+        python python/util.py csv_unzip $sim $sim_1 $sim_2
         
         # Run on left column
         ./covq/covq \
-        --train tr_1.csv cb_out.csv cw_map_out.csv \
-        --test sim_1.csv cb_out.csv cw_map_out.csv sim_1.out \
+        --train $tr_1 $cb $cw \
+        --test $sim_1 $cb $cw $out_1 \
         --dim 1 --nsplits $nsplits --bep $bep --lbg-eps $lbg_eps --cv-disp $cv_disp
 
         if [ $? -ne 0 ]
@@ -35,8 +49,8 @@ do
 
         # Run on right column
         ./covq/covq \
-        --train tr_2.csv cb_out.csv cw_map_out.csv \
-        --test sim_2.csv cb_out.csv cw_map_out.csv sim_2.out \
+        --train $tr_2 $cb $cw \
+        --test $sim_2 $cb $cw $out_2 \
         --dim 1 --nsplits $nsplits --bep $bep --lbg-eps $lbg_eps --cv-disp $cv_disp
 
         if [ $? -ne 0 ]
@@ -45,13 +59,13 @@ do
         fi
 
         # Zip the results back into 2 column csv
-        python python/util.py csv_zip sim_1.out sim_2.out corr_$k.$i$j.out
+        python python/util.py csv_zip $out_1 $out_2 corr_$k.$i$j.out
         
         # Write rho to out file
         echo -n $k.$i$j >> $snr_out
         echo -n ', ' >> $snr_out
         # Get python to measure SNR
-        python python/run.py get_snr corr_$k.$i$j.sim corr_$k.$i$j.out >> $snr_out
+        python python/run.py get_snr $sim corr_$k.$i$j.out >> $snr_out
     done
 done
 
@@ -59,14 +73,21 @@ k=1
 i=0
 j=0
 
+train=$train
+sim=$sim
+if [ ! -e train ] || [ ! -e sim ]
+    then echo "Cannot find train/sim files, rho=$k.$i$j"
+    exit 1
+fi
+
 # Split the sets into single columns
-python python/util.py csv_unzip corr_$k.$i$j.train tr_1.csv tr_2.csv
-python python/util.py csv_unzip corr_$k.$i$j.sim sim_1.csv sim_2.csv
+python python/util.py csv_unzip corr_$k.$i$j.train $tr_1 $tr_2
+python python/util.py csv_unzip $sim $sim_1 $sim_2
 
 # Run on left column
 ./covq/covq \
---train tr_1.csv cb_out.csv cw_map_out.csv \
---test sim_1.csv cb_out.csv cw_map_out.csv sim_1.out \
+--train $tr_1 $cb $cw \
+--test $sim_1 $cb $cw $out_1 \
 --dim 1 --nsplits $nsplits --bep $bep --lbg-eps $lbg_eps --cv-disp $cv_disp
 
 if [ $? -ne 0 ]
@@ -76,8 +97,8 @@ fi
 
 # Run on right column
 ./covq/covq \
---train tr_2.csv cb_out.csv cw_map_out.csv \
---test sim_2.csv cb_out.csv cw_map_out.csv sim_2.out \
+--train $tr_2 $cb $cw \
+--test $sim_2 $cb $cw $out_2 \
 --dim 1 --nsplits $nsplits --bep $bep --lbg-eps $lbg_eps --cv-disp $cv_disp
 
 if [ $? -ne 0 ]
@@ -86,7 +107,7 @@ if [ $? -ne 0 ]
 fi
 
 # Zip the results back into 2 column csv
-python python/util.py csv_zip sim_1.out sim_2.out corr_$k.$i$j.out
+python python/util.py csv_zip $out_1 $out_2 corr_$k.$i$j.out
 
 # Write rho to out file
 echo -n $k.$i$j >> $snr_out
@@ -94,6 +115,5 @@ echo -n ', ' >> $snr_out
 # Get python to measure SNR
 python python/run.py get_snr corr_$k.$i$j.sim corr_$k.$i$j.out >> $snr_out
 
-if [ $i == 9 ] && [ $j == 9 ]
-    then k=1
-fi
+# Clean up
+rm $cw $cb $out_1 $out_2 $sim_1 $sim_2 $tr_1 $tr_2
